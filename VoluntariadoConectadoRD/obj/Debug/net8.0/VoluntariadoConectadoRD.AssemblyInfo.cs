9581b4f85bcf89786e2e85d7227701770e8e76bc
SamuelPina <samuelpinapolanco@gmail.com>
2025-07-30 18:56:32 -0400

    Update build artifacts before merge

--- VoluntariadoConectadoRD/obj/Debug/net8.0/VoluntariadoConectadoRD.AssemblyInfo.cs
+++ VoluntariadoConectadoRD/obj/Debug/net8.0/VoluntariadoConectadoRD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f8caa925bfb172f42d328136afb77417248b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
