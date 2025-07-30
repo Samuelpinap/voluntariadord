f8650fcc987f4a9281184d5a265bf01366eb7163
Oscar Jiménez <ojimenez6@est.unibe.edu.do>
2025-07-29 21:37:01 -0400

    Create Oportunities.cs

---
+++ VoluntariadoConectadoRD/Models/Oportunities.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace VoluntariadoApi.Models
+{
+    public class Oportunidad
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(200)]
+        public string Titulo { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(1000)]
+        public string Descripcion { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(150)]
+        public string Ubicacion { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(50)]
+        public string Tipo { get; set; } = string.Empty;
+        
+        [Required]
+        public DateTime FechaInicio { get; set; }
+        
+        [Required]
+        public DateTime FechaFin { get; set; }
+    }
+}
