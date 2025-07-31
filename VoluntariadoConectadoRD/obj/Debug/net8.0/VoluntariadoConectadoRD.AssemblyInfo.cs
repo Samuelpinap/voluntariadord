67e3d07b20e026bd07751da3525fa26b5de39850
isantos <idelossantos4@est.unibe.edu.do>
2025-07-30 21:20:41 -0400

    commit

--- VoluntariadoConectadoRD/obj/Debug/net8.0/VoluntariadoConectadoRD.AssemblyInfo.cs
+++ VoluntariadoConectadoRD/obj/Debug/net8.0/VoluntariadoConectadoRD.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c0eb28f84fc6b7472c252dad7990f54bf020b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53bf80ee58298aa44be8b9d970eaec2fe716962")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
