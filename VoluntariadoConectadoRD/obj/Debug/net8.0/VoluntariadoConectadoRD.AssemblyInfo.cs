e736030da405fce3f698dd226b209584fa39bad7
SamuelPina <samuelpinapolanco@gmail.com>
2025-08-12 13:57:26 -0400

    Feat: seed

--- VoluntariadoConectadoRD/obj/Debug/net8.0/VoluntariadoConectadoRD.AssemblyInfo.cs
+++ VoluntariadoConectadoRD/obj/Debug/net8.0/VoluntariadoConectadoRD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15d7e4dd198e0d21ddf2f920a9b61184d06234a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6155d31ff7802b7c19e161da56ca2dc1c212dbcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoluntariadoConectadoRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
